b09015a7a891adac38f37202b4224be084303f04
Dennis.Gamboa <dennis.gamboa@jalasoft.com>
2023-09-25 15:51:14 -0400

    added test for notepad++

---
+++ notepad/notepad_plus_plus/test_notepad/Scenarios/test_notepad.feature
@@ -0,0 +1,6 @@
+﻿Feature: Adding a note to Notepad++
+
+Scenario: Add a note to Notepad++
+    Given Notepad++ is open
+    When I add a note with the text "Hello, Notepad++"
+    Then I should see the note in Notepad++
---
+++ notepad/notepad_plus_plus/test_notepad/Script/steps_notepad.py
@@ -0,0 +1,30 @@
+﻿@given("Notepad++ is open")
+def step_impl():
+  TestedApps.notepad.Run(1, True)
+
+@when("I add a note with the text {arg}")
+def step_impl(param1):
+  notepad_ = Aliases.notepad_
+  win_notepad = notepad_.wndNotepad
+  win_notepad.DblClick(1475, 105)
+  win_notepad.Scintilla.Keys(param1)
+  win_notepad.Close()
+
+@then("I should see the note in Notepad++")
+def step_impl():
+  expected_result = "Hello, Notepad++"
+  TestedApps.notepad.Run(1, True)
+  notepad_ = Aliases.notepad_
+  win_notepad = notepad_.wndNotepad
+  win_notepad.Scintilla.Keys("^a")
+  win_notepad.Scintilla.Keys("^c")
+  actual_result = Sys.Clipboard
+  if actual_result == expected_result:
+    Log.Message("Note is added succesfully, Test PASSED")
+  else:
+    message = "Actual Result: " + actual_result
+    Log.Error("Note is not added, Test FAILED " + message)
+  
+  win_notepad.Tab.Click(1538, 18)
+  notepad_.dlgSave.btnNo.ClickButton()  
+  win_notepad.Close()
